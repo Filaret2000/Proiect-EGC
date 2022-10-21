1e161c25beaa74d5be310c396ea958346ef54625
Filaret-Niculai Crainiciuc <c.filaret2000@yahoo.com>
2022-10-21 10:45:53 +0300

    Project creation

---
+++ repos/Crainiciuc FIlaret-Niculai/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crainiciuc_FIlaret_Niculai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
